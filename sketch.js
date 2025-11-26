e95533491c0966cb8bbffec4f2d79ca867bd9983
harveypearce <rvp526@york.ac.uk>
2025-11-26 12:51:38 +0000

    first attempt

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup(){
+
+    createCanvas(600,600);
+
+}
+
+function polarBear(){
+    fill(255);
+circle(100,100,75);
+circle(100,50,50);
+fill(200,90,0);
+square(100,100,10);
+square(100,120,10);
+square(100,80,10);
+line(20,100,100,100)
+}
+function draw(){
+    background(220);
+      polarBear() 
+}
